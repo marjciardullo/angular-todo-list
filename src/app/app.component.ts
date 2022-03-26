fb0cbb166617985568e5bcdbf28d7c0e3e42c39b
marjciardullo <marjoryeramirez@gmail.com>
2022-03-25 21:00:04 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  item: string = '';
+  list: string[] = [];
+
+  adicionarItem() {
+    this.list.push(this.item);
+  }
+
+  removerItem() {}
+}
